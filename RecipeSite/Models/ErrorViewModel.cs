736a8e4c8ecb07ccaa7408feae71e228a64d9281
Student <Student@DESKTOP-GAFDJHT>
2026-05-20 10:15:18 +0300

    Створено базовий MVC шаблон проєкту

---
+++ RecipeSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
